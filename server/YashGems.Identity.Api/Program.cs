e9cc7ce73fe27788f74465c6b53d494714e1ffc0
maaitlunghau <chunhau.py@gmail.com>
2026-04-03 19:01:41 +0700

    chore(api): remove open API package & config

--- server/YashGems.Identity.Api/Program.cs
+++ server/YashGems.Identity.Api/Program.cs
@@ -13,10 +13,6 @@ using YashGems.Identity.Infrastructure.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
 // 1. Cấu hình DbContext (Dùng MySQL)
 var connectionString = builder.Configuration.GetConnectionString("MySQL");
 builder.Services.AddDbContext<IdentityDbContext>(options =>
@@ -88,33 +84,9 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // 
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
