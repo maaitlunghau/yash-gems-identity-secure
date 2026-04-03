6b703c7629363ce9a86a4f0e73f36c2fea1016ff
maaitlunghau <chunhau.py@gmail.com>
2026-04-03 16:56:01 +0700

    chore(infras): install neccessary packages for JWT

--- server/YashGems.Identity.Infrastructure/obj/Debug/net10.0/YashGems.Identity.Infrastructure.AssemblyInfo.cs
+++ server/YashGems.Identity.Infrastructure/obj/Debug/net10.0/YashGems.Identity.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YashGems.Identity.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3c307e57097901d97f7b0fde390f47cc6613c4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f9b2bce55b02e339a1df987d1ea69bc3b68292a")]
 [assembly: System.Reflection.AssemblyProductAttribute("YashGems.Identity.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YashGems.Identity.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
