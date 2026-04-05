817b22e0a50fac63e220b7ee5668b059da372a69
maaitlunghau <chunhau.py@gmail.com>
2026-04-05 14:07:36 +0700

    feat: update port URL for axios

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://localhost:5001/api',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://localhost:5021/api',
     headers: {
         'Content-Type': 'application/json',
     },
