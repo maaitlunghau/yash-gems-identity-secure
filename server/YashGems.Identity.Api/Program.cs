32ca751c8c3490843aae071148d0f9be61b6d0e9
maaitlunghau <chunhau.py@gmail.com>
2026-04-05 14:09:08 +0700

    feat(api): configure CORS to allow frontend send request

--- server/YashGems.Identity.Api/Program.cs
+++ server/YashGems.Identity.Api/Program.cs
@@ -95,6 +95,17 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+               .AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowCredentials();
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -134,6 +145,8 @@ app.MapControllers();
 
 app.UseAuthentication();
 app.UseAuthorization();
+
+app.UseCors("AllowAll");
 
 app.MapGet("/", () => Results.Redirect("/swagger"));
 
